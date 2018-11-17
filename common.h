d16110e3717c972241c245bd72b4454da171c0d4
Paul Nowoczynski <pauln@localhost.localdomain>
2018-11-17 12:43:04 -0500

    Improve common.h definitions add chain link
    Document and fixup various definitions in common.h.
    
    Add a structure for the metablock chain link.
    
    Add a copyright block file.

--- common.h
+++ common.h
@@ -1,3 +1,9 @@
+/* Copyright (C) NIOVA Systems, Inc - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Paul Nowoczynski <00pauln00@gmail.com> 2018
+ */
+
 #ifndef NIOVA_COMMON_H
 #define NIOVA_COMMON_H 1
 
@@ -7,66 +13,137 @@
 #define NBBY 8
 #endif
 
-#define MB_CHAIN_LINK_BITS         256 // Support sha256
-#define MB_CHAIN_LINK_BYTES        (MB_CHAIN_LINK_BITS / NBBY)
-#define MB_CHAIN_LINK_UINT64_BYTES (MB_CHAIN_LINK_BITS / sizeof(uint64_t))
-
-#define MB_HEADER_IO_SIZE_BYTES 512
-
-#define PBLK_BLK_SIZE_BITS  17
-#define PBLK_BLK_SIZE_BYTES (1ULL << PBLK_BLK_SIZE_BITS)
-
-#define CHUNK_SIZE_BITS  33
-#define CHUNK_SIZE_BYTES (1ULL << CHUNK_SIZE_BITS)
-
-#define VBLK_ADDR_SIZE_BITS  12
-#define VBLK_ADDR_SIZE_BYTES (1ULL << VBLK_SIZE_BITS)
-
-#define VBLK_ADDR_BITS         (CHUNK_SIZE_BITS - VBLK_ADDR_SIZE_BITS)
-#define VBLK_ADDR_RUN_LEN_BITS (PBLK_BLK_SIZE_BITS - VBLK_ADDR_SIZE_BITS)
-#define VBLK_PBLK_IDX     VBLK_RUN_LEN_BITS
-
-#define NIOVA_MAX_DEVICE_SIZE_BITS 47 //128 TiB
-#define PBLK_ADDR_BITS (NIOVA_MAX_DEVICE_SIZE_BITS - PBLK_BLK_SIZE_BITS)
-
-#define NIOVA_TXN_BITS 48
-
-#define NIOVA_EC_TYPE_BITS 5
-#define NIOVA_EC_POS_BITS  5
-
-#define MB_HEADER_SIZE_BYTES 4096
-
-#define MB_VBLK_ADDR_SIZE_BYTES 8
-#define MB_DATA_PBLK_SIZE_BYTES 12
-
-#define MB_CHKSUM_BYTES_PER_PBLK 128
-#define MB_DATA_PBLKS_PER_CHKSUM_PBLK \
-    (PBLK_BLK_SIZE_BYTES / MB_CHKSUM_BYTES_PER_PBLK)
-
-#define MB_MAX_NUM_CHKSUM_BLKS_BITS \
-    (((1 << MB_MAX_DATA_PBLKS_BITS) / MB_DATA_PBLKS_PER_CHKSUM_PBLK) + \
-     (1 << MB_MAX_DATA_PBLKS_BITS) % MB_DATA_PBLKS_PER_CHKSUM_PBLK ? 1 : 0)
-
-#define MB_MAX_NUM_CHKSUM_BLKS (1U << MB_MAX_NUM_CHKSUM_BLKS_BITS)
-
 #define PACKED __attribute__((packed))
 
 #define COMPILE_TIME_ASSERT(cond) \
     ((void)sizeof(char[1 - 2*!(condition)]))
 
-/* Max number of data pblks which can be housed in a metablock.  This comes
- * from the following calculation:
- * ((PBLK_BLK_SIZE_BYTES - MB_HEADER_SIZE_BYTES) /
- *  (MB_VBLK_ADDR_SIZE_BYTES + MB_DATA_PBLK_SIZE_BYTES))
+/**
+ * The maximum single device size that is currently supported.
  */
-#define MB_MAX_DATA_PBLKS 6348
+#define NIOVA_MAX_DEVICE_SIZE_BITS 47 //128 TiB
+
+/**
+ * Number of bits used to represent a transaction number.
+ */
+#define NIOVA_TXN_BITS 48
+
+/**
+ * Erasure coding type and position maximums.
+ */
+#define NIOVA_EC_TYPE_BITS 5
+#define NIOVA_EC_POS_BITS  5
+
+/**
+ * Metablock chain link hash sizes.
+ */
+#define MB_CHAIN_LINK_HASH_BITS         256 // Support sha256
+#define MB_CHAIN_LINK_HASH_BYTES        (MB_CHAIN_LINK_BITS / NBBY)
+#define MB_CHAIN_LINK_HASH_UINT64_BYTES (MB_CHAIN_LINK_BITS / sizeof(uint64_t))
+
+/**
+ * The Metablock header I/O size used both for the chain link and the
+ * header contents.
+ */
+#define MB_HEADER_IO_SIZE_BYTES 1024
+
+/**
+ * Physical block size is currently fixed at 128KiB.  Physical blocks (aka
+ * 'pblks') are used for every persistent allocation type (data, metablocks,
+ * checksum blocks, etc.).
+ */
+#define PBLK_SIZE_BITS  17 // 128KiB
+#define PBLK_SIZE_BYTES (1ULL << PBLK_SIZE_BITS)
+
+/**
+ * Number of total physical blocks on a single device.
+ */
+#define PBLK_ADDR_BITS (NIOVA_MAX_DEVICE_SIZE_BITS - PBLK_SIZE_BITS)
+
+/**
+ * Virtual block device chunk size.  The 'chunk' is a logical division or
+ * partitioning of the virtual block device and serves as a unit of
+ * distribution, residency, and management.
+ */
+#define VBLKDEV_CHUNK_SIZE_BITS  33 // 8GiB
+#define VBLKDEV_CHUNK_SIZE_BYTES (1ULL << CHUNK_SIZE_BITS)
+
+/**
+ * Virtual block address size is 4KiB.
+ */
+#define VBLK_ADDR_SIZE_BITS  12 // 4KiB
+#define VBLK_ADDR_SIZE_BYTES (1ULL << VBLK_SIZE_BITS)
+
+/**
+ * Virtual block address identifier bits represents the address space size
+ * of a chunk in vblk address size.  In other words, the number of virtual
+ * blocks which can reside in a chunk.
+ */
+#define VBLK_ADDR_BITS (CHUNK_SIZE_BITS - VBLK_ADDR_SIZE_BITS)
+
+/**
+ * Virtual block address run length bits is used to represent coalesced virtual
+ * blocks which are residing in the same pblk.
+ */
+#define VBLK_ADDR_RUN_LEN_BITS (PBLK_SIZE_BITS - VBLK_ADDR_SIZE_BITS)
+
+/**
+ * Virtual block index into a physical block.
+ */
+#define VBLK_PBLK_IDX VBLK_ADDR_RUN_LEN_BITS
+
+/**
+ * Metablock header size.  This header includes two components:  chain link,
+ * and header data.
+ */
+#define MB_HEADER_SIZE_BYTES 4096
+
+/**
+ * Metablock virtual address entry size in bytes.
+ */
+#define MB_VBLK_ADDR_ENTRY_SIZE_BYTES 8
+/**
+ * Metablock physical address entry size in bytes
+ */
+#define MB_DPBLK_ENTRY_SIZE_BYTES 12
+
+/**
+ * Metablock checksum bytes per physical data block.
+ */
+#define MB_CHKSUM_BYTES_PER_DPBLK 128
+
+/**
+ * Metablock:  data physical blocks per checksum physical block.
+ */
+#define MB_DPBLKS_PER_CPBLK (PBLK_SIZE_BYTES / MB_CHKSUM_BYTES_PER_PBLK)
+
+#define MB_DPBLKS_PER_CPBLK_HARDCODED 1024
+
+/**
+ * Max number of data pblks which can be housed in a metablock.
+ */
+#define MB_MAX_DPBLKS \
+    ((PBLK_SIZE_BYTES - MB_HEADER_SIZE_BYTES) / \
+     (MB_VBLK_ADDR_ENTRY_SIZE_BYTES + MB_DPBLK_ENTRY_SIZE_BYTES))
+
+#define MB_MAX_DPBLKS_HARDCODED 6348
+
+/**
+ * Maximum number of checksum physical blocks which can be referenced by a
+ * metablock.
+ */
+#define MB_MAX_CPBLKS                                   \
+    ((MB_MAX_DPBLKS / MB_DPBLKS_PER_CPBLK) +            \
+     (MB_MAX_DPBLKS % MB_DPBLKS_PER_CPBLK ? 1 : 0))
+
+#define MB_MAX_CPBLKS_HARDCODED 7
 
 static inline void
 common_compile_time_asserts(void)
 {
-    COMPILE_TIME_ASSERT(MB_MAX_DATA_PBLKS ==
-                        ((PBLK_BLK_SIZE_BYTES - MB_HEADER_SIZE_BYTES) /
-                         (MB_VBLK_ADDR_SIZE_BYTES + MB_DATA_PBLK_SIZE_BYTES)));
+    COMPILE_TIME_ASSERT(MB_MAX_DPBLKS == MB_MAX_DPBLKS_HARDCODED);
+    COMPILE_TIME_ASSERT(MB_MAX_CPBLKS == MB_MAX_CPBLKS_HARDCODED);
+    COMPILE_TIME_ASSERT(MB_DPBLKS_PER_CPBLK == MB_DPBLKS_PER_CPBLK_HARDCODED);
 }
 
 #endif //NIOVA_COMMON_H
--- metablock.h
+++ metablock.h
@@ -1,3 +1,9 @@
+/* Copyright (C) NIOVA Systems, Inc - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Paul Nowoczynski <00pauln00@gmail.com> 2018
+ */
+
 #ifndef NIOVA_METABLOCK_H
 #define NIOVA_METABLOCK_H 1
 
@@ -18,8 +24,6 @@ struct mb_vblk_addr
              mvba__pad:VBLK_INFO_PAD_BITS;
 } PACKED;
 
-#define MB_VBLK_ADDR_SIZE_BYTES 8
-
 struct mb_data_pblk
 {
     uint64_t mdpb_txnid:NIOVA_TXN_BITS,
@@ -30,16 +34,16 @@ struct mb_data_pblk
              mdpb__pad1;
 } PACKED;
 
-#define MB_DATA_PBLK_SIZE_BYTES 12
-
 struct mb_hash
 {
+    uint32_t          mh_type;
+    uint32_t          mh__pad;
     union
     {
-        unsigned char mh_bytes[MB_CHAIN_LINK_BYTES];
-        uint64_t      mh_uint64[MB_CHAIN_LINK_UINT64_BYTES];
+        unsigned char mh_bytes[MB_CHAIN_LINK_HASH_BYTES];
+        uint64_t      mh_uint64[MB_CHAIN_LINK_HASH_UINT64_BYTES];
     };
-};
+} PACKED;
 
 struct mb_chksum_blk
 {
@@ -48,45 +52,63 @@ struct mb_chksum_blk
     struct mb_hash mcb_hash;
 } PACKED;
 
-#define MB_CHKSUM_BLK_SIZE_BYTES (MB_CHAIN_LINK_BYTES + sizeof(uint64_t))
+#define MB_CHKSUM_BLK_SIZE_BYTES (MB_CHAIN_LINK_HASH_BYTES + sizeof(uint64_t))
 
-/**
- * Persistent link to 'next' metablock.
- * @mclh_hash:  contains the hash the next MB's contents
- */
 struct mb_chain_link_hash
 {
     uint32_t       mclh_type;
-    uint32_t       mclh_version;
+    uint32_t       mclh__pad;
     struct mb_hash mclh_hash;
 } PACKED;
 
 struct mb_chain_link
 {
-    uint64_t mcl_pblk_id:PBLK_ADDR_BITS;
-    struct mb_chain_link_hash mcl_link_hash; //this hash must match the next
-                                             //mb in the chain
+    uint64_t                  mcl_pblk_id:PBLK_ADDR_BITS,
+                              mcl__pad;
+    struct mb_chain_link_hash mcl_link_hash;
 } PACKED;
 
-struct mb_header
+struct mb_header_chain_link
 {
+    uint64_t             mbcl_magic;
+    struct mb_hash       mbcl_hash;
+    // <-- START: mbcl_hash coverage -->
+    struct mb_chain_link mbcl_self_mb;
+    struct mb_chain_link mbcl_next_mb;
+    uint32_t             mbcl_type; //end of the chain?
+    uint32_t             mbcl__pad0;
+    // <-- END: mbcl_hash coverage -->
+} PACKED;
+
+struct mb_header_data
+{
+    uint64_t             mbh_magic;
+    struct mb_hash       mbh_self_hash; // must match mbcl_self_mb
+    // <-- START: mbh_self_hash coverage -->
     uint32_t             mbh_version;
     uint32_t             mbh__pad;
+    uint32_t             mbh_type; //general metablock?
+    uint32_t             mbh__pad0;
     uint32_t             mbh_vba_crc;
     uint64_t             mbh_self_pblk_id:PBLK_ADDR_BITS; //needs to go into
                                                           //the hash
-    uint64_t             mbh_num_data_pblks:MB_MAX_DATA_PBLKS_BITS;
+    uint32_t             mbh_num_data_pblks;
+    uint32_t             mbh_num_vbas;
     struct mb_chksum_blk mbh_cblks[MB_MAX_NUM_CHKSUM_BLKS];
-    struct mb_chain_link mbh_next_mb;
-    struct mb_hash       mbh_self_mb;
+    // <-- mbh_self_hash coverage continues to the entire pblk ... >
 } PACKED;
 
 struct mb_header_persistent
 {
     union
     {
-        struct mb_header mbhp_header;
-        unsigned char    mbhp_buffer[MB_HEADER_IO_SIZE_BYTES];
+        struct mb_header_chain_link mbhp_chain_link_header;
+        unsigned char    mbhp_chain_link_header_data[MB_HEADER_IO_SIZE_BYTES];
+    };
+    union
+    {
+        struct mb_header_data mbhp_mb_header;
+        unsigned char         mbhp_mb_header_data[MB_HEADER_IO_SIZE_BYTES];
     };
 } PACKED;
 
@@ -103,7 +125,7 @@ mb_compile_time_checks(void)
                         MB_CHKSUM_BLK_SIZE_BYTES);
 
     COMPILE_TIME_ASSERT(sizeof(struct mb_header_persistent) ==
-                        MB_HEADER_IO_SIZE_BYTES);
+                        MB_HEADER_IO_SIZE_BYTES * 2);
 }
 
 #endif
