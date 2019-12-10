c222387ae9bb2421c3f05ca7402b667f2415e209
Paul Nowoczynski <00pauln00@gmail.com>
2019-12-10 15:24:41 -0500

    Niova-ctl improvements

--- src/niova-ctl.c
+++ src/niova-ctl.c
@@ -7,13 +7,15 @@
 #include "log.h"
 #include "common.h"
 #include "niosd_io.h"
+#include "superblock.h"
 
-#define OPTS "mh"
+#define OPTS "mhi"
 
 struct niova_ctl
 {
     const char         *nc_dev_name;
-    unsigned int        nc_dev_mkosd:1;
+    unsigned int        nc_dev_mkosd:1,
+                        nc_dev_dump_info:1;
     struct niosd_device nc_ndev;
 };
 
@@ -21,9 +23,10 @@ static void
 niova_ctl_help(const int error)
 {
     fprintf(errno ? stderr : stdout,
-            "niova_ctl [-h] [-m] <device-name>\n"
-            "\t -h  help - prints this help message.\n"
-            "\t -m  mk-osd - initializes device for niova OSD.\n");
+            "niova_ctl [-h] [-m] [-i] <device-name>\n"
+            "\t -h  (help)   Prints this help message.\n"
+            "\t -m  (mk-osd) Initializes device as a NIOVA OSD.\n"
+            "\t -i  (info)   Get device information.\n");
 
     exit(error);
 
@@ -50,6 +53,9 @@ niova_ctl_getopt(int argc, char **argv, struct niova_ctl *nc)
         case 'h':
             niova_ctl_help(0);
             break;
+        case 'i':
+            nc->nc_dev_dump_info = 1;
+            break;
         default:
             niova_ctl_help(EINVAL);
             break;
@@ -57,6 +63,30 @@ niova_ctl_getopt(int argc, char **argv, struct niova_ctl *nc)
     }
 
     nc->nc_dev_name = argv[argc - 1];
+}
+
+static void
+niova_ctl_dump_dev_info(const struct niova_ctl *nc)
+{
+    if (!nc)
+        return;
+
+    const struct niosd_device *ndev = &nc->nc_ndev;
+
+    fprintf(stdout,
+            "device_name:        %s\n"
+            "status:             %s\n"
+            "niosd_uuid:         %lx.%lx\n"
+            "physical_size:      %ld\n"
+            "provisioned_size:   %ld\n"
+            "superblock_version: %d\n",
+            ndev->ndev_name,
+            niosd_dev_status_2_string(ndev->ndev_status),
+            sb_2_niosd_id(ndev->ndev_sb, false),
+            sb_2_niosd_id(ndev->ndev_sb, true),
+            sb_2_phys_size_bytes(ndev->ndev_sb),
+            sb_2_prov_size_bytes(ndev->ndev_sb),
+            sb_2_version(ndev->ndev_sb));
 }
 
 int
@@ -78,6 +108,8 @@ main(int argc, char **argv)
     if (rc)
         exit(rc);
 
+    if (nc.nc_dev_dump_info)
+        niova_ctl_dump_dev_info(&nc);
 
     rc = niosd_device_close(&nc.nc_ndev);
 
