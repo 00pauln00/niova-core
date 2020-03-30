20b2a66ef598876e633d2d9a02f8b8dd1f2b21a5
Paul Nowoczynski <00pauln00@gmail.com>
2020-03-30 12:23:29 -0400

    Add 'local' mode to random test
    Local mode uses a privately allocated random buffer.  This is meant to
    recreate the usage of the raft client.  I did try briefy to use
    setstate_r() to replicate the contents of one generator instance and
    pass it to another, however, I had mixed results.  In some select cases
    I was able to end up with 2 generators producing the same stream of
    randoms but this depended on the iteration number in which the
    replication was done.

--- test/random-test.c
+++ test/random-test.c
@@ -69,6 +69,7 @@ main(int argc, char **argv)
 
     unsigned int val = 0;
 
+    fprintf(stdout, "-- Global Generator --\n");
     for (uint64_t i = 0; i < seqno; i++)
     {
         val = random_get();
@@ -78,4 +79,76 @@ main(int argc, char **argv)
     }
 
     fprintf(stdout, "seqno=%lu rand-val=%08u\n", seqno, val);
+
+    fprintf(stdout, "-- Local Generator --\n");
+
+    struct random_data rand_data = {0};
+    char rand_state_buf[RANDOM_STATE_BUF_LEN] = {0};
+
+    if (initstate_r(1040071U, rand_state_buf,
+                    RANDOM_STATE_BUF_LEN, &rand_data))
+	SIMPLE_LOG_MSG(LL_FATAL, "initstate_r() failed: %s", strerror(errno));
+
+    for (uint64_t i = 0; i < seqno; i++)
+    {
+        if (random_r(&rand_data, (int *)&val))
+            SIMPLE_LOG_MSG(LL_FATAL, "random_r() failed: %s", strerror(errno));
+
+        if (verbose)
+        {
+            fprintf(stdout, "\ti=%04lx seqno=%lu rand-val=%08u\n",
+                    i, seqno, val);
+        }
+    }
+    fprintf(stdout, "seqno=%lu rand-val=%08u\n", seqno, val);
+
+    fprintf(stdout, "-- Local Generator Clone --\n");
+
+    if (initstate_r(1040071U, rand_state_buf,
+                    RANDOM_STATE_BUF_LEN, &rand_data))
+	SIMPLE_LOG_MSG(LL_FATAL, "initstate_r() failed: %s", strerror(errno));
+
+    struct random_data rand_data_clone = {0};
+    char tmp[RANDOM_STATE_BUF_LEN] = {0};
+    char rand_state_buf_clone[RANDOM_STATE_BUF_LEN] = {0};
+
+    if (initstate_r(1040071U, tmp,
+                    RANDOM_STATE_BUF_LEN, &rand_data_clone))
+	SIMPLE_LOG_MSG(LL_FATAL, "initstate_r() failed: %s", strerror(errno));
+
+    val = 0;
+    unsigned int clone_val = 0;
+    for (uint64_t i = 0; i < seqno; i++)
+    {
+        if (i == seqno / 2)
+        {
+//            memcpy(&rand_data_clone, &rand_data, sizeof(struct random_data));
+            memcpy(rand_state_buf_clone, rand_state_buf, RANDOM_STATE_BUF_LEN);
+            if (setstate_r(rand_state_buf_clone, &rand_data_clone))
+                SIMPLE_LOG_MSG(LL_FATAL, "setstate_r() failed: %s",
+                               strerror(errno));
+        }
+
+        if (random_r(&rand_data, (int *)&val))
+            SIMPLE_LOG_MSG(LL_FATAL, "random_r() failed: %s", strerror(errno));
+
+        if (i >= seqno / 2)
+            if (random_r(&rand_data_clone, (int *)&clone_val))
+                SIMPLE_LOG_MSG(LL_FATAL, "random_r() failed: %s",
+                               strerror(errno));
+
+        if (verbose)
+        {
+            if (i >= seqno / 2)
+                fprintf(stdout,
+                        "\ti=%04lx seqno=%lu rand-val=%08u\tclone-val=%08u\n",
+                        i, seqno, val, clone_val);
+            else
+                fprintf(stdout, "\ti=%04lx seqno=%lu rand-val=%08u\n",
+                        i, seqno, val);
+        }
+    }
+
+    fprintf(stdout, "seqno=%lu rand-val=%08u clone-val=%08u\n",
+            seqno, val, clone_val);
 }
