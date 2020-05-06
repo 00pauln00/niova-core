a79eae9a498469ae8c8827d5bfc74c8a83fc4fa4
Paul Nowoczynski <00pauln00@gmail.com>
2020-05-06 16:51:16 -0400

    Ctl-interface cmd helper script
    ctl-interface-cmd-to-all.sh copies a command file into the ctl-interface
    directories which are named as UUIDs.  The script has an option to
    iterate 'i' times each second and the ability to launch terminals to
    edit the cmd file and/or watch the output.
    
    Usage:  scripts/ctl-interface-cmd-to-all.sh [-d ctl-dir (/tmp/.niova/)]
    	[-i iterations (1)] [-e (launch edit terminal)]
    	[-w (launch watch terminal)] <cmd-file>

---
+++ scripts/ctl-interface-cmd-to-all.sh
@@ -0,0 +1,107 @@
+#!/bin/sh
+
+CTL_DIR=/tmp/.niova/
+ITERATIONS=1
+VERBOSE=""
+WATCH=0
+EDIT=0
+
+while getopts ":vd:i:we" opt; do
+  case ${opt} in
+    d )
+        CTL_DIR=$OPTARG
+        if [ ! -d $CTL_DIR ]
+        then
+            echo "ctl-interface path '$CTL_DIR' does not exist or is not a directory."
+            exit
+        fi
+        ;;
+    e )
+        EDIT=1
+        ;;
+    i )
+        ITERATIONS=$OPTARG
+        RES=`echo $ITERATIONS | egrep ^[1-9]$\|[1-9][0-9]+$`
+        if [ $? -ne 0 ]
+        then
+            echo "Iteration value '$ITERATIONS' does not appear to be numeric."
+            exit
+        fi
+        ;;
+    w )
+        WATCH=1
+        ;;
+    v )
+        VERBOSE="v"
+        ;;
+    \? )
+        echo "Invalid option: '$OPTARG'" 1>&2
+        echo ""
+        echo \
+            "Usage:  $0 [-d ctl-dir (${CTL_DIR})] [-i iterations (${ITERATIONS})] [-e (launch edit terminal)] [-w (launch watch terminal)] <cmd-file>"
+      ;;
+    : )
+      echo "Invalid option: '$OPTARG' requires an argument" 1>&2
+      ;;
+  esac
+done
+
+# Adjust $# to account for 'getopts'
+shift $((OPTIND -1))
+
+if [ $# -lt 1 ]
+then
+    echo \
+        "Usage:  $0 [-d ctl-dir (${CTL_DIR})] [-i iterations (${ITERATIONS})] <cmd-file>"
+    exit 1
+fi
+
+CMD_FILE=$1
+if [ ! -f $CMD_FILE ]
+then
+    echo "cmd-file '$CMD_FILE' does not exist or is not a regular file."
+    exit
+fi
+
+RES=`egrep ^OUTFILE $CMD_FILE`
+if [ $? -ne 0 ]
+then
+    echo "cmd-file '$CMD_FILE' does contain the OUTFILE directive"
+    exit
+fi
+
+RES=`egrep ^OUTFILE[[:space:]][/][/a-zA-Z0-9_] $CMD_FILE`
+if [ $? -ne 0 ]
+then
+    echo "cmd-file '$CMD_FILE' has malformed OUTFILE directive"
+    exit
+fi
+
+OUT_FILE=`egrep ^OUTFILE $CMD_FILE | awk '{print $2}' | sed s/\\\///g`
+
+if [ $WATCH -eq 1 ]
+then
+    REGEX_PATH=${CTL_DIR}[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}/output/$OUT_FILE
+    CMD="watch -n 1 find ${CTL_DIR} -regextype posix-egrep -regex \"${REGEX_PATH}\" -exec cat {} \\;"
+    gnome-terminal -- $CMD
+fi
+
+if [ $EDIT -eq 1 ]
+then
+    gnome-terminal -- vim $CMD_FILE
+fi
+
+while [ $ITERATIONS -gt 0 ]
+do
+    for i in `find ${CTL_DIR} | \
+        egrep [a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$`;
+    do
+        cp -f${VERBOSE} $CMD_FILE $i/input;
+    done
+
+    let ITERATIONS=$ITERATIONS-1
+    if [ $ITERATIONS -gt 0 ]
+    then
+        sleep 1
+    fi
+done
