3a0c124cd15fb1beda7e7f1516b446b0803b5365
Paul Nowoczynski <00pauln00@gmail.com>
2020-06-15 17:24:25 -0400

    Fix disable-send.cmd
    It was enabling, not disabling.

--- scripts/ctl-interface-cmds/disable-send.cmd
+++ scripts/ctl-interface-cmds/disable-send.cmd
@@ -1,4 +1,4 @@
-APPLY net_recv_enabled@true
-#APPLY net_recv_enabled@false
-WHERE /ctl_svc_nodes/uuid@77f6031c-5284-11ea-81f5-90324b2d1e89
+#APPLY net_recv_enabled@true
+APPLY net_recv_enabled@false
+WHERE /ctl_svc_nodes/uuid@9da4e924-aa7d-11ea-b4b5-90324b2d1e89
 OUTFILE /error.txt
