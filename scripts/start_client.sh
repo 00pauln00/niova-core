f0bd86cec094edf89e043a0fd687f62fd8a666fb
Kit Westneat <kit.westneat@gmail.com>
2020-08-17 17:16:52 -0400

    Improve debug script

--- scripts/start_client.sh
+++ scripts/start_client.sh
@@ -1,5 +1,7 @@
 RAFT=$(basename $NIOVA_LOCAL_CTL_SVC_DIR | cut -d . -f 1)
 CLIENT=$(ls $NIOVA_LOCAL_CTL_SVC_DIR | grep raft_client | head -1 | cut -d. -f1)
 
+GDB="gdb -ex=r --args"
+
 echo NIOVA_LOG_LEVEL=5 ./raft-client -r $RAFT -u $CLIENT
-NIOVA_LOG_LEVEL=5 ./raft-client -r $RAFT -u $CLIENT
+NIOVA_LOG_LEVEL=5 $GDB ./raft-client -r $RAFT -u $CLIENT
