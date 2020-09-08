5a65127150c30511c1dd83c4f0b9aff2c901afa7
Paul Nowoczynski <00pauln00@gmail.com>
2020-09-08 17:53:35 -0400

    Add async support to raft_server_backend_posix.c
    Simple implementation of .rib_backend_sync.  Modify the other API calls
    to remove unconditional synchronous operations.

--- src/raft_server.c
+++ src/raft_server.c
@@ -2002,6 +2002,8 @@ raft_server_timerfd_cb(struct raft_instance *ri)
         break;
     }
 
+//XXX if leader and followers still have sync ops pending, wake up at a higher
+//    frequency
     raft_server_timerfd_settime(ri);
 }
 
