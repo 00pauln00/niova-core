f4dcd89f8f81cb46010fef42577ec8dc1751c013
Paul Nowoczynski <pauln@niova.io>
2020-09-10 17:01:38 -0400

    RocksDB raft backend supports async mode
    Implement .rib_backend_sync for the rocksDB backend.  The function
    rsbr_sync() is called from sync_thread context and therefore does not
    use the writebatch contained in the raft_instance_rocks_db.
    
    A new store type has been added,
    RAFT_INSTANCE_STORE_ROCKSDB_PERSISTENT_APP.  Using this store type will
    cause rsb_sm_get_last_applied_kv_idx() to be called which causes raft
    startup to bypass SM applies which have already been placed into
    RocksDB.  Apps which use RAFT_INSTANCE_STORE_ROCKSDB are assumed to be
    in-memory and will have entries reapplied at startup.

--- src/pumice_db.c
+++ src/pumice_db.c
@@ -924,7 +924,7 @@ PmdbExec(const char *raft_uuid_str, const char *raft_instance_uuid_str,
 
     return raft_server_instance_run(raft_uuid_str, raft_instance_uuid_str,
                                     pmdb_sm_handler,
-                                    RAFT_INSTANCE_STORE_ROCKSDB,
+                                    RAFT_INSTANCE_STORE_ROCKSDB_PERSISTENT_APP,
                                     use_synchronous_writes, &pmdbCFT);
 }
 
