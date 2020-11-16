aeee5ea61f78ab6c56f1ae3d96c582d91329c857
Paul Nowoczynski <00pauln00@gmail.com>
2020-11-16 16:19:50 -0500

    raft_server_rocksdb_release_cf_table() called in scope
    Prior to this patch, the cf_table was being released from inside
    rsbr_destroy().  This prevented reused of the table within the same
    instance of raft_server_instance_run().
    
    This patch causes the release of the cf table to occur in the same scope
    as where it was initialized / allocated.

--- src/pumice_db.c
+++ src/pumice_db.c
@@ -911,10 +911,14 @@ PmdbExec(const char *raft_uuid_str, const char *raft_instance_uuid_str,
             return rc;
     }
 
-    return raft_server_instance_run(raft_uuid_str, raft_instance_uuid_str,
-                                    pmdb_sm_handler,
-                                    RAFT_INSTANCE_STORE_ROCKSDB_PERSISTENT_APP,
-                                    use_synchronous_writes, &pmdbCFT);
+    rc = raft_server_instance_run(raft_uuid_str, raft_instance_uuid_str,
+                                  pmdb_sm_handler,
+                                  RAFT_INSTANCE_STORE_ROCKSDB_PERSISTENT_APP,
+                                  use_synchronous_writes, &pmdbCFT);
+
+    raft_server_rocksdb_release_cf_table(&pmdbCFT);
+
+    return rc;
 }
 
 /**
