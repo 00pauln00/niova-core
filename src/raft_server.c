3b99a6b5862c4ebd0a4aff888b14510123b3a8bd
Paul Nowoczynski <pauln@niova.io>
2020-12-09 11:11:18 -0500

    raft_bulk_recovery: lessen assert in raft_server startup
    raft_server_backend_setup_last_applied() was asserting on some internal
    values causing the recovery initialization to assert.  The removed
    asserts were a bit too pedantic w/out and there value has been reduced
    since their inception due to improvements in the raft initialization
    code.

--- src/raft_server.c
+++ src/raft_server.c
@@ -3949,12 +3949,8 @@ raft_server_backend_setup_last_applied(struct raft_instance *ri,
                                        raft_entry_idx_t last_applied_idx,
                                        crc32_t last_applied_cumulative_crc)
 {
-    // Assert some setup / bootup context items.
-    NIOVA_ASSERT(ri && ri->ri_last_applied_idx == -1 &&
-                 ri->ri_commit_idx == -1 &&
-                 ri->ri_last_applied_cumulative_crc == 0 &&
-                 (raft_instance_is_booting(ri) ||
-                  raft_instance_is_recovering(ri)));
+    NIOVA_ASSERT(ri && (raft_instance_is_booting(ri) ||
+                        raft_instance_is_recovering(ri)));
 
     ri->ri_last_applied_idx = last_applied_idx;
     ri->ri_last_applied_cumulative_crc = last_applied_cumulative_crc;
