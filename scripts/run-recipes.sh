1de3abcf80ed357b52095c823868b3a14b7478d4
Manisha Salve <manisha@paroscale.com>
2021-01-06 08:59:04 +0000

    Run ansible from holon directory.

--- scripts/run-recipes.sh
+++ scripts/run-recipes.sh
@@ -14,4 +14,4 @@ export ANSIBLE_LOOKUP_PLUGINS=$HOLON_LIBS
 export PYTHONPATH=$HOLON_LIBS
 export NIOVA_BIN_PATH=$BIN_PATH
 
-ansible-playbook -e 'srv_port=4000' -e npeers=$NPEERS -e dir_path=$LOG_PATH -e 'client_port=14000' -e 'recipe=leader_overthrow.yml' -e 'backend_type=pumicedb' $NIOVA_BIN_PATH/holon/ansible/holon.yml
+ansible-playbook -e 'srv_port=4000' -e npeers=$NPEERS -e dir_path=$LOG_PATH -e 'client_port=14000' -e 'recipe=leader_overthrow.yml' -e 'backend_type=pumicedb' holon.yml
