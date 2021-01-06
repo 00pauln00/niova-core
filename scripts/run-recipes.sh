506253e214605c5c35379192f42cbe7889d129e5
Manisha Salve <manisha@paroscale.com>
2021-01-06 07:19:00 +0000

    Run the holon recipes through shell script.

---
+++ scripts/run-recipes.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+HOLON_LIBS = $1
+BIN_PATH = $2
+LOG_PATH = $3
+NPEERS = $4
+
+echo "Holon lib $HOLON_LIBS"
+echo "All executatbles at: $BIN_PATH"
+echo "holon log path: $LOG_PATH"
+echo "Npeers: $NPEERS"
+
+export ANSIBLE_LOOKUP_PLUGINS=$HOLON_LIBS
+export PYTHONPATH=$HOLON_LIBS
+export NIOVA_BIN_PATH=$BIN_PATH
+
+sh ansible-playbook -e 'srv_port=4000' -e npeers=$NPEERS -e dir_path=$LOG_PATH -e 'client_port=14000' -e 'recipe=leader_overthrow.yml' -e 'backend_type=pumicedb' $NIOVA_BIN_PATH/holon/ansible/holon.yml
