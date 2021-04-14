9e779f87f795984662b42446b605152ec0d6be12
VishKapare10 <abhay10kapare@gmail.com>
2021-04-14 11:40:50 +0000

    Implemented zomato_data_pumicedb_application.

---
+++ test/zomato_pmdb_app/zomatoapplib/zomatoapplib.go
@@ -0,0 +1,12 @@
+package zomatoapplib
+
+type Zomato_App struct {
+
+	Restaurant_id int64
+	Restaurant_name string
+        City string
+        Cuisines string
+        Ratings_text string
+        Votes int64
+
+}
