f4b7f456d28fdef906577584fd77e5cef705be8c
VishKapare10 <abhay10kapare@gmail.com>
2021-04-16 14:14:12 +0000

    Made changes as per review comments.

--- test/zomato_pmdb_app/zomato_app_client/zomato_app_client.go
+++ test/zomato_pmdb_app/zomato_app_client/zomato_app_client.go
@@ -1,19 +1,18 @@
 package main
-
 import (
-        "encoding/csv"
-        "fmt"
-	"time"
-	"unsafe"
-        "io"
-	"strings"
-        "log"
-        "os"
-	"bufio"
-	"strconv"
-        "gopmdblib/goPmdbClient"
-	"github.com/satori/go.uuid"
-        "zomatoapp/zomatoapplib"
+  "encoding/csv"
+  "fmt"
+  "time"
+  "unsafe"
+  "io"
+  "strings"
+  "log"
+  "os"
+  "bufio"
+  "strconv"
+  "gopmdblib/goPmdbClient"
+  "github.com/satori/go.uuid"
+  "zomatoapp/zomatoapplib"
 )
 
 /*
@@ -21,136 +20,135 @@ import (
 */
 import "C"
 
-func Zomato_app_apply(struct_app *zomatoapplib.Zomato_App, pmdb unsafe.Pointer){
+func update(struct_app *zomatoapplib.Zomato_Data, pmdb unsafe.Pointer){
 
-		//Generate app_uuid.
-		app_uuid := uuid.NewV4().String()
+	//Generate app_uuid.
+	app_uuid := uuid.NewV4().String()
 
-		//Create rncui string.
-		rncui := app_uuid+":0:0:0:0"
+	//Create rncui string.
+	rncui := app_uuid+":0:0:0:0"
 
-		//Open file for storing key, rncui in append mode.
-                file, err := os.OpenFile("key_rncui_data.txt", os.O_APPEND|os.O_WRONLY, 0644)
-                    if err != nil {
-                        log.Println(err)
-                }
-                defer file.Close()
+	//Open file for storing key, rncui in append mode.
+	file, err := os.OpenFile("key_rncui_data.txt", os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+	   log.Println(err)
+	}
+	defer file.Close()
 
-		//Append key_rncui in file.
-		rest_id_string := PumiceDBClient.GoIntToString(int(struct_app.Restaurant_id))
-		if _, err := file.WriteString("key, rncui = "+rest_id_string+"  "+rncui+"\n"); err != nil {
-		log.Fatal(err)
-		}
+	//Append key_rncui in file.
+	rest_id_string := strconv.Itoa(int(struct_app.Restaurant_id))
+	_, err_write := file.WriteString("key, rncui = "+rest_id_string+"  "+rncui+"\n")
+	if err_write != nil {
+	   log.Fatal(err)
+	}
 
-		//Perform write operation.
-		PumiceDBClient.PmdbClientWrite(struct_app, pmdb, rncui)
+	//Perform write operation.
+	PumiceDBClient.PmdbClientWrite(struct_app, pmdb, rncui)
 
 }
 
-func Parse_file_and_apply(pmdb unsafe.Pointer, filename string){
+func parse_file_and_update(pmdb unsafe.Pointer, filename string){
 
-		//Open the file.
-		csvfile, err := os.Open(filename)
+	//Open the file.
+	csvfile, err := os.Open(filename)
+	if err != nil {
+	   log.Fatalln("Couldn't open the csv file", err)
+	}
+
+	//Parse the file.
+	// Skip first row (line)
+	row1, err := bufio.NewReader(csvfile).ReadSlice('\n')
+	if err != nil {
+	   log.Fatalln("error")
+	}
+	_, err = csvfile.Seek(int64(len(row1)), io.SeekStart)
+	if err != nil {
+	   log.Fatalln("error")
+	}
+
+	//Read remaining rows.
+	r := csv.NewReader(csvfile)
+
+	//Iterate through the records.
+	for {
+		//Read each record from csv.
+		record, err := r.Read()
+		if err == io.EOF {
+		   break
+		}
 		if err != nil {
-		       log.Fatalln("Couldn't open the csv file", err)
+		   log.Fatal(err)
 		}
 
-		//Parse the file.
-		// Skip first row (line)
-		row1, err := bufio.NewReader(csvfile).ReadSlice('\n')
+		//Typecast Restaurant_id to int64.
+		Restaurant_id_struct, err := strconv.ParseInt(record[0], 10, 64)
 		if err != nil {
-		   log.Fatalln("error")
+		   fmt.Println("Error occured in typecasting Restaurant_id to int64")
 		}
-		_, err = csvfile.Seek(int64(len(row1)), io.SeekStart)
+
+		//Typecast Votes to int64.
+		Votes_struct, err := strconv.ParseInt(record[5], 10, 64)
 		if err != nil {
-		   log.Fatalln("error")
+		   fmt.Println("Error occured in typecasting Votes to int64")
 		}
 
-		//Read remaining rows.
-		r := csv.NewReader(csvfile)
+		//Fill the Zomato_App structure.
+		struct_data := zomatoapplib.Zomato_Data {
+				Restaurant_id: Restaurant_id_struct,
+				Restaurant_name: record[1],
+				City: record[2],
+				Cuisines: record[3],
+				Ratings_text: record[4],
+				Votes: Votes_struct,
+				}
+		update(&struct_data, pmdb)
 
-		//Iterate through the records.
-		for {
-			//Read each record from csv.
-			record, err := r.Read()
-			if err == io.EOF {
-				break
-			}
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			//Typecast Restaurant_id to int64.
-			Restaurant_id_struct, err := strconv.ParseInt(record[0], 10, 64)
-			if err != nil {
-				fmt.Println("Error occured in typecasting Restaurant_id to int64")
-			}
-
-			//Typecast Votes to int64.
-			Votes_struct, err := strconv.ParseInt(record[5], 10, 64)
-			if err != nil {
-				fmt.Println("Error occured in typecasting Votes to int64")
-			}
-
-			//Fill the Zomato_App structure.
-			struct_data := zomatoapplib.Zomato_App {
-					Restaurant_id: Restaurant_id_struct,
-					Restaurant_name: record[1],
-					City: record[2],
-					Cuisines: record[3],
-					Ratings_text: record[4],
-					Votes: Votes_struct,
-					}
-			Zomato_app_apply(&struct_data, pmdb)
-
-		}
+	}
 }
 
 
-func Zomato_app_read(pmdb unsafe.Pointer, key string, read_rncui string){
+func get(pmdb unsafe.Pointer, key string, read_rncui string){
 
 
-			var reply_len int64
+	var reply_len int64
 
-			//Typecast key into int64.
-			key_int64,_ := strconv.ParseInt(key, 10, 64)
+	//Typecast key into int64.
+	key_int64,_ := strconv.ParseInt(key, 10, 64)
 
-			//Fill the Zomato_App structure.
-			struct_read := zomatoapplib.Zomato_App {
-				    Restaurant_id: key_int64,
-			}
+	//Fill the Zomato_App structure.
+	struct_read := zomatoapplib.Zomato_Data {
+		    Restaurant_id: key_int64,
+	}
 
-			//Get size of the structure struct_data.
-			struct_len := PumiceDBClient.GetStructSize(struct_read)
+	//Get size of the structure struct_data.
+	struct_len := PumiceDBClient.GetStructSize(struct_read)
 
-			fmt.Println("Length of the struct_read: ", struct_len)
+	fmt.Println("Length of the struct_read: ", struct_len)
 
-			//Allocate C memory to store the value of the result.
-			struct_buf := C.malloc(65536)
+	//Allocate C memory to store the value of the result.
+	struct_buf := C.malloc(65536)
 
-			//Perform read operation.
-			rc := PumiceDBClient.PmdbClientRead(struct_read, pmdb, read_rncui, struct_buf, 65536, &reply_len)
-			if rc < 0 {
-				fmt.Println("Read request failed, error: ", rc)
-				fmt.Println("Reply length returned is: ", reply_len)
-			} else {
-				fmt.Println("Read the return data now")
-				fmt.Println("Length of reply buffer:",reply_len)
-				read_data := &zomatoapplib.Zomato_App{}
-				PumiceDBClient.Decode(struct_buf, read_data, reply_len)
+	//Perform read operation.
+	rc := PumiceDBClient.PmdbClientRead(struct_read, pmdb, read_rncui, struct_buf, 65536, &reply_len)
+	if rc < 0 {
+		fmt.Println("Read request failed, error: ", rc)
+		fmt.Println("Reply length returned is: ", reply_len)
+	} else {
+		fmt.Println("Read the return data now")
+		fmt.Println("Length of reply buffer:",reply_len)
+		read_data := &zomatoapplib.Zomato_Data{}
+		PumiceDBClient.Decode(struct_buf, read_data, reply_len)
 
-				fmt.Println("\nData received after read request:")
-				fmt.Println("Restaurant id = ",read_data.Restaurant_id)
-				fmt.Println("Restaurant name = ",read_data.Restaurant_name)
-				fmt.Println("City = ",read_data.City)
-				fmt.Println("Cuisines = ",read_data.Cuisines)
-				fmt.Println("Ratings_text = ",read_data.Ratings_text)
-				fmt.Println("Votes = ",read_data.Votes)
+		fmt.Println("\nData received after read request:")
+		fmt.Println("Restaurant id = ",read_data.Restaurant_id)
+		fmt.Println("Restaurant name = ",read_data.Restaurant_name)
+		fmt.Println("City = ",read_data.City)
+		fmt.Println("Cuisines = ",read_data.Cuisines)
+		fmt.Println("Ratings_text = ",read_data.Ratings_text)
+		fmt.Println("Votes = ",read_data.Votes)
 
-			}
-			C.free(struct_buf)
-
-
+	}
+	C.free(struct_buf)
 }
 
 
@@ -189,10 +187,9 @@ func main(){
 	fmt.Println("Filename:", filename)
 
         //Perform write operation.
-	Parse_file_and_apply(pmdb, filename)
+	parse_file_and_update(pmdb, filename)
 
-	for {
-
+	for{
 		fmt.Print("Enter operation (write/read/readall/exit): ")
 		ops,_ := input.ReadString('\n')
 		ops = strings.Replace(ops, "\n", "", -1)
@@ -208,17 +205,17 @@ func main(){
 			rest_id_string := cmdline_prms[0]
 			Restaurant_id_struct, err := strconv.ParseInt(rest_id_string, 10, 64)
 			if err != nil {
-				fmt.Println("Error occured in typecasting Restaurant_id to int64")
+			   fmt.Println("Error occured in typecasting Restaurant_id to int64")
 			}
 
 			//Typecast Votes to int64.
 			Votes_struct, err := strconv.ParseInt(cmdline_prms[5], 10, 64)
 			if err != nil {
-				fmt.Println("Error occured in typecasting Votes to int64")
+			   fmt.Println("Error occured in typecasting Votes to int64")
 			}
 
 			//Fill the Zomato_App structure.
-			struct_data_cmdline := zomatoapplib.Zomato_App {
+			struct_data_cmdline := zomatoapplib.Zomato_Data {
 					Restaurant_id: Restaurant_id_struct,
 					Restaurant_name: cmdline_prms[1],
 					City: cmdline_prms[2],
@@ -227,9 +224,10 @@ func main(){
 					Votes: Votes_struct,
 					}
 
-			Zomato_app_apply(&struct_data_cmdline, pmdb)
+			//Perform write operation.
+			update(&struct_data_cmdline, pmdb)
 
-		} else if ops == "read" {
+		} else if ops == "read"{
 
 			fmt.Println("\nEnter key(Restuarant_id), rncui in the format - key_rncui (underscore seperated):")
 			read_prms,_ := input.ReadString('\n')
@@ -243,21 +241,21 @@ func main(){
 			fmt.Println("rucui:", rncui)
 
 			//Perform read operation.
-			Zomato_app_read(pmdb, key, rncui)
+			get(pmdb, key, rncui)
 
 		} else if ops == "readall"{
 
 			f, err := os.Open("key_rncui_data.txt")
 
-		        if err != nil {
+			if err != nil {
 			   log.Fatal(err)
-		        }
+			}
 
-		        defer f.Close()
+			defer f.Close()
 
-		        scanner := bufio.NewScanner(f)
+			scanner := bufio.NewScanner(f)
 
-		        for scanner.Scan() {
+			for scanner.Scan() {
 
 				    rall_data := strings.Split(scanner.Text()," ")
 
@@ -267,15 +265,15 @@ func main(){
 				    fmt.Println("\nPerforming read for all key,values...key, rncui = ", rall_key,rall_rncui)
 
 				    //Perform read operation for every key and rncui associated with it.
-				    Zomato_app_read(pmdb, rall_key, rall_rncui)
-		        }
+				    get(pmdb, rall_key, rall_rncui)
+			}
 
-		        if err := scanner.Err(); err != nil {
+			if err := scanner.Err(); err != nil {
 				    log.Fatal(err)
-		        }
+			}
 		} else if ops == "exit"{
 			os.Exit(0)
-		} else {
+		} else{
 			fmt.Println("\nEnter valid operation: write/read/readall/exit")
 		}
 	}
