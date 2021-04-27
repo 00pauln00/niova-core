f98003d54a0184dc15d8ab373196f797eba5db3a
Paul Nowoczynski <pauln@niova.io>
2021-04-27 16:47:56 -0400

    Methodize pmdb go server library calls
    WriteKV(), ReadKV(), CopyDataToBuffer(), etc. are now presented as
    methods, as is PmdbStartServer() whose method is called 'Run()'.
    
    I added an interface, PmdbServerAPI, to replace the function callback
    types.  This interface can be implemented by the user's server-side
    implementation.  In this patch, the dictionary app has been given a
    object, DictionaryServer, which implements both 'Apply()' and 'Read()'.
    Therefore, an instance of DictionaryServer may fulfill the requirements
    of being an implementation of PmdbServerAPI.

--- go/pumiceDB/examples/dictionary_app/server/pumice-dict-server.go
+++ go/pumiceDB/examples/dictionary_app/server/pumice-dict-server.go
@@ -1,14 +1,15 @@
 package main
+
 import (
+	"flag"
 	"fmt"
-	"unsafe"
+	"log"
 	"strconv"
 	"strings"
-	"flag"
-	"niova/go-pumicedb-lib/common"
-	"niova/go-pumicedb-lib/server"
+	"unsafe"
+
 	"dictapplib/lib"
-	"log"
+	"niova/go-pumicedb-lib/server"
 )
 
 /*
@@ -17,46 +18,54 @@ import (
 import "C"
 
 var seqno = 0
-var raft_uuid_go string
-var peer_uuid_go string
 var word_map map[string]int
-// Use the default column family 
+
+// Use the default column family
 var colmfamily = "PMDBTS_CF"
+
+type DictionaryServer struct {
+	raftUuid       string
+	peerUuid       string
+	columnFamilies string
+	pso            *PumiceDBServer.PmdbServerObject
+}
 
 //split the string and add each word in the word-map
 func split_and_write_to_word_map(text string) {
 	words := strings.Fields(text)
 	// Store words and its count in the map
-    for _,word := range words{
-        word_map[word]++
-    }
+	for _, word := range words {
+		word_map[word]++
+	}
 }
 
-func dict_apply(app_id unsafe.Pointer, input_buf unsafe.Pointer,
-			input_buf_sz int64, pmdb_handle unsafe.Pointer) {
+func (dso *DictionaryServer) Apply(app_id unsafe.Pointer,
+	input_buf unsafe.Pointer, input_buf_sz int64,
+	pmdb_handle unsafe.Pointer) {
 
 	fmt.Println("Dictionary app server: Apply request received")
 
 	/* Decode the input buffer into dictionary structure format */
 	//apply_dict := DictAppLib.DictAppDecodebuf(input_buf, input_buf_sz)
 	apply_dict := &DictAppLib.Dict_app{}
-	PumiceDBCommon.Decode(input_buf, apply_dict, input_buf_sz)
+
+	dso.pso.Decode(input_buf, apply_dict, input_buf_sz)
 
 	fmt.Println("Key passed by client: %s", apply_dict.Dict_text)
 
 	/* Split the words and create map for word to frequency */
 	split_and_write_to_word_map(apply_dict.Dict_text)
 
-	/*
-     	Iterate over word_map and write work as key and frequency
-	    as value to pmdb.
-	*/
+	/* Iterate over word_map and write work as key and frequency as value
+	 * to pmdb.
+	 */
 	for word, count := range word_map {
 		go_key_len := len(word)
 		var prev_value string
 
 		//Lookup the key first
-		prev_result := PumiceDBServer.PmdbLookupKey(word, int64(go_key_len), prev_value, colmfamily)
+		prev_result := dso.pso.LookupKey(word, int64(go_key_len),
+			prev_value, colmfamily)
 		fmt.Println("Previous value of the key: ", prev_result)
 
 		if prev_result != "" {
@@ -70,29 +79,32 @@ func dict_apply(app_id unsafe.Pointer, input_buf unsafe.Pointer,
 		value_len := PumiceDBServer.GoStringLen(value)
 
 		fmt.Println("Write the KeyValue by calling PmdbWriteKV")
+
 		//Write word and frequency as value to Pmdb
-		PumiceDBServer.PmdbWriteKV(app_id, pmdb_handle, word, int64(go_key_len), value,
-				 int64(value_len), colmfamily)
+		dso.pso.WriteKV(app_id, pmdb_handle, word, int64(go_key_len),
+			value, int64(value_len), colmfamily)
 
 		//Delete the word entry once written in the pumicedb
 		delete(word_map, word)
 	}
 }
 
-func dict_read(app_id unsafe.Pointer, request_buf unsafe.Pointer,
-            request_bufsz int64, reply_buf unsafe.Pointer, reply_bufsz int64) int64 {
+func (dso *DictionaryServer) Read(app_id unsafe.Pointer,
+	request_buf unsafe.Pointer, request_bufsz int64,
+	reply_buf unsafe.Pointer, reply_bufsz int64) int64 {
 	fmt.Println("Dictionary App: Read request received")
 
 	//Decode the request structure sent by client.
 	req_dict := &DictAppLib.Dict_app{}
-	PumiceDBCommon.Decode(request_buf, req_dict, request_bufsz)
+	dso.pso.Decode(request_buf, req_dict, request_bufsz)
 
 	fmt.Println("Key passed by client: %s", req_dict.Dict_text)
 
 	key_len := len(req_dict.Dict_text)
 
-	/* Pass the work as key to PmdbReadKV and get the value from pumicedb */
-	result := PumiceDBServer.PmdbReadKV(app_id, req_dict.Dict_text, int64(key_len), colmfamily)
+	/* Pass the word as key to ReadKV and get the value from pumicedb */
+	result := dso.pso.ReadKV(app_id, req_dict.Dict_text, int64(key_len),
+		colmfamily)
 
 	/* typecast the output to int */
 	word_frequency := 0
@@ -106,39 +118,51 @@ func dict_read(app_id unsafe.Pointer, request_buf unsafe.Pointer,
 	}
 
 	result_dict := DictAppLib.Dict_app{
-		Dict_text: req_dict.Dict_text,
+		Dict_text:   req_dict.Dict_text,
 		Dict_wcount: word_frequency,
 	}
 
 	//Copy the encoded result in reply_buffer
-	reply_size := PumiceDBServer.PmdbCopyDataToBuffer(result_dict, reply_buf)
+	reply_size := dso.pso.CopyDataToBuffer(result_dict, reply_buf)
 
 	fmt.Println("Reply size is: ", reply_size)
 	return reply_size
 }
 
-func pmdb_dict_app_getopts() {
+func dictionaryServerNew() *DictionaryServer {
+	dso := &DictionaryServer{}
 
-	flag.StringVar(&raft_uuid_go, "raft", "NULL", "raft uuid")
-	flag.StringVar(&peer_uuid_go, "peer", "NULL", "peer uuid")
+	flag.StringVar(&dso.raftUuid, "raft", "NULL", "raft uuid")
+	flag.StringVar(&dso.peerUuid, "peer", "NULL", "peer uuid")
 
 	flag.Parse()
-	fmt.Println("Raft UUID: ", raft_uuid_go)
-	fmt.Println("Peer UUID: ", peer_uuid_go)
+	fmt.Println("Raft UUID: ", &dso.raftUuid)
+	fmt.Println("Peer UUID: ", &dso.peerUuid)
+
+	dso.columnFamilies = colmfamily
+
+	return dso
 }
 
 func main() {
-	//Parse the cmdline parameters
-	pmdb_dict_app_getopts()
+	//Parse the cmdline parameters and generate new dictionary object
+	dso := dictionaryServerNew()
+
+	// Initialize the internal pmdb-server-object pointer
+	dso.pso = &PumiceDBServer.PmdbServerObject{
+		ColumnFamilies: colmfamily,
+		RaftUuid:       dso.raftUuid,
+		PeerUuid:       dso.peerUuid,
+		PmdbAPI:        dso,
+	}
 
 	//Create empty word map
 	word_map = make(map[string]int)
 
-	//Initialize the dictionary application callback functions
-	cb := &PumiceDBServer.PmdbCallbacks{
-		ApplyCb: dict_apply,
-		ReadCb:  dict_read,
-	}
+	// Start the pmdb server
+	err := dso.pso.Run()
 
-	PumiceDBServer.PmdbStartServer(raft_uuid_go, peer_uuid_go, colmfamily, cb)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
--- go/pumiceDB/server/PumiceDBServer.go
+++ go/pumiceDB/server/PumiceDBServer.go
@@ -1,11 +1,13 @@
 package PumiceDBServer
 
 import (
+	"errors"
 	"fmt"
-	"niova/go-pumicedb-lib/common"
 	"reflect"
 	"strconv"
 	"unsafe"
+
+	"niova/go-pumicedb-lib/common"
 )
 
 /*
@@ -23,15 +25,16 @@ import "C"
 
 import gopointer "github.com/mattn/go-pointer"
 
-type PmdbApplyCallback func(unsafe.Pointer, unsafe.Pointer, int64,
-	unsafe.Pointer)
-type PmdbReadCallback func(unsafe.Pointer, unsafe.Pointer, int64,
-	unsafe.Pointer, int64) int64
+type PmdbServerAPI interface {
+	Apply(unsafe.Pointer, unsafe.Pointer, int64, unsafe.Pointer)
+	Read(unsafe.Pointer, unsafe.Pointer, int64, unsafe.Pointer, int64) int64
+}
 
-//Callback function for pmdb apply and read
-type PmdbCallbacks struct {
-	ApplyCb PmdbApplyCallback
-	ReadCb  PmdbReadCallback
+type PmdbServerObject struct {
+	PmdbAPI        PmdbServerAPI
+	RaftUuid       string
+	PeerUuid       string
+	ColumnFamilies string // XXX should be an array of strings
 }
 
 type charsSlice []*C.char
@@ -83,13 +86,14 @@ func goApply(app_id *C.struct_raft_net_client_user_id, input_buf unsafe.Pointer,
 	user_data unsafe.Pointer) {
 
 	//Restore the golang function pointers stored in PmdbCallbacks.
-	gcb := gopointer.Restore(user_data).(*PmdbCallbacks)
+	gcb := gopointer.Restore(user_data).(*PmdbServerObject)
 
 	//Convert buffer size from c data type size_t to golang int64.
 	input_buf_sz_go := CToGoInt64(input_buf_sz)
 
 	//Calling the golang Application's Apply function.
-	gcb.ApplyCb(unsafe.Pointer(app_id), input_buf, input_buf_sz_go, pmdb_handle)
+	gcb.PmdbAPI.Apply(unsafe.Pointer(app_id), input_buf, input_buf_sz_go,
+		pmdb_handle)
 }
 
 //export goRead
@@ -98,14 +102,15 @@ func goRead(app_id *C.struct_raft_net_client_user_id, request_buf unsafe.Pointer
 	user_data unsafe.Pointer) int64 {
 
 	//Restore the golang function pointers stored in PmdbCallbacks.
-	gcb := gopointer.Restore(user_data).(*PmdbCallbacks)
+	gcb := gopointer.Restore(user_data).(*PmdbServerObject)
 
 	//Convert buffer size from c data type size_t to golang int64.
 	request_bufsz_go := CToGoInt64(request_bufsz)
 	reply_bufsz_go := CToGoInt64(reply_bufsz)
 
 	//Calling the golang Application's Read function.
-	return gcb.ReadCb(unsafe.Pointer(app_id), request_buf, request_bufsz_go, reply_buf, reply_bufsz_go)
+	return gcb.PmdbAPI.Read(unsafe.Pointer(app_id), request_buf,
+		request_bufsz_go, reply_buf, reply_bufsz_go)
 }
 
 /**
@@ -115,17 +120,23 @@ func goRead(app_id *C.struct_raft_net_client_user_id, request_buf unsafe.Pointer
  * @cf: Column Family
  * @cb: PmdbAPI callback funcs.
  */
-func PmdbStartServer(raft_uuid string, peer_uuid string, cf string,
-	cb *PmdbCallbacks) {
-
+func PmdbStartServer(pso *PmdbServerObject) error {
+	if pso == nil {
+		return errors.New("Null server object parameter")
+	}
+	if pso.RaftUuid == "" || pso.PeerUuid == "" {
+		return errors.New("Raft and/or peer UUIDs were not specified")
+	}
 	/*
 	 * Convert the raft_uuid and peer_uuid go strings into C strings
 	 * so that we can pass these to C function.
 	 */
 
-	raft_uuid_c := GoToCString(raft_uuid)
+	raft_uuid_c := GoToCString(pso.RaftUuid)
+	defer FreeCMem(raft_uuid_c)
 
-	peer_uuid_c := GoToCString(peer_uuid)
+	peer_uuid_c := GoToCString(pso.PeerUuid)
+	defer FreeCMem(peer_uuid_c)
 
 	cCallbacks := C.struct_PmdbAPI{}
 
@@ -138,7 +149,7 @@ func PmdbStartServer(raft_uuid string, peer_uuid string, cf string,
 	 * Store gostring to byte array.
 	 * Don't forget to append the null terminating character.
 	 */
-	cf_byte_arr := []byte(cf + "\000")
+	cf_byte_arr := []byte(pso.ColumnFamilies + "\000")
 
 	cf_name := make(charsSlice, len(cf_byte_arr))
 	cf_name[0] = (*C.char)(C.CBytes(cf_byte_arr))
@@ -148,18 +159,32 @@ func PmdbStartServer(raft_uuid string, peer_uuid string, cf string,
 	cf_array := (**C.char)(unsafe.Pointer(sH.Data))
 
 	// Create an opaque C pointer for cbs to pass to PmdbStartServer.
-	opa_ptr := gopointer.Save(cb)
+	opa_ptr := gopointer.Save(pso)
 	defer gopointer.Unref(opa_ptr)
 
 	// Starting the pmdb server.
-	C.PmdbExec(raft_uuid_c, peer_uuid_c, &cCallbacks, cf_array, 1, true, opa_ptr)
+	rc := C.PmdbExec(raft_uuid_c, peer_uuid_c, &cCallbacks, cf_array, 1,
+		true, opa_ptr)
 
-	//Free the C memory
-	FreeCMem(raft_uuid_c)
-	FreeCMem(peer_uuid_c)
+	if rc != 0 {
+		return fmt.Errorf("PmdbExec() returned %d", rc)
+	}
 
+	return nil
 }
 
+// Method version of PmdbStartServer()
+func (pso *PmdbServerObject) Run() error {
+	return PmdbStartServer(pso)
+}
+
+// Export the common decode method via the server object
+func (*PmdbServerObject) Decode(input unsafe.Pointer, output interface{},
+	len int64) {
+	PumiceDBCommon.Decode(input, output, len)
+}
+
+// Try to find a key in RocksDB
 func PmdbLookupKey(key string, key_len int64, value string,
 	go_cf string) string {
 
@@ -208,6 +233,13 @@ func PmdbLookupKey(key string, key_len int64, value string,
 	return result
 }
 
+// Public method of PmdbLookupKey
+func (*PmdbServerObject) LookupKey(key string, key_len int64, value string,
+	go_cf string) string {
+	return PmdbLookupKey(key, key_len, value, go_cf)
+}
+
+// Write a key-value pair into pumice DB
 func PmdbWriteKV(app_id unsafe.Pointer, pmdb_handle unsafe.Pointer, key string,
 	key_len int64, value string, value_len int64, gocolfamily string) {
 
@@ -237,6 +269,15 @@ func PmdbWriteKV(app_id unsafe.Pointer, pmdb_handle unsafe.Pointer, key string,
 	FreeCMem(C_value)
 }
 
+// Public method of PmdbWriteKV
+func (*PmdbServerObject) WriteKV(app_id unsafe.Pointer,
+	pmdb_handle unsafe.Pointer, key string,
+	key_len int64, value string, value_len int64, gocolfamily string) {
+	PmdbWriteKV(app_id, pmdb_handle, key, key_len, value, value_len,
+		gocolfamily)
+}
+
+// Read a KV from pumiceDb
 func PmdbReadKV(app_id unsafe.Pointer, key string,
 	key_len int64, gocolfamily string) string {
 
@@ -251,6 +292,13 @@ func PmdbReadKV(app_id unsafe.Pointer, key string,
 	return go_value
 }
 
+// Public method of PmdbReadKV
+func (*PmdbServerObject) ReadKV(app_id unsafe.Pointer, key string,
+	key_len int64, gocolfamily string) string {
+	return PmdbReadKV(app_id, key, key_len, gocolfamily)
+}
+
+// Copy data from the user's application into the pmdb reply buffer
 func PmdbCopyDataToBuffer(ed interface{}, buffer unsafe.Pointer) int64 {
 	var key_len int64
 	//Encode the structure into void pointer.
@@ -260,4 +308,10 @@ func PmdbCopyDataToBuffer(ed interface{}, buffer unsafe.Pointer) int64 {
 	C.memcpy(buffer, encoded_key, C.size_t(key_len))
 
 	return key_len
+}
+
+// Public method version of PmdbCopyDataToBuffer
+func (*PmdbServerObject) CopyDataToBuffer(ed interface{},
+	buffer unsafe.Pointer) int64 {
+	return PmdbCopyDataToBuffer(ed, buffer)
 }
