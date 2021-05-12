787050add0c8960d0a26882242eca5e206bc0281
Paul Nowoczynski <pauln@niova.io>
2021-05-12 10:45:54 -0400

    new crc functions for 24-bit and t10-dif

--- src/include/crc32.h
+++ src/include/crc32.h
@@ -14,8 +14,12 @@ typedef uint32_t crc32_t;
 #if defined(__x86_64__)
 extern uint32_t
 crc_pcl(const unsigned char *buffer, int len, unsigned int crc_init);
+
+extern uint16_t
+crc_t10dif_pcl(uint16_t crc_init, const unsigned char *buffer, size_t len);
 #endif
 
 #define niova_crc crc_pcl
+#define niova_t10dif_crc crc_t10dif_pcl
 
 #endif
