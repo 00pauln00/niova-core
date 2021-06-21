f6901e68d71f3df7f180d35d2c9f1c60dcde2e29
Manisha Salve <manisha@paroscale.com>
2021-06-21 13:07:34 +0000

    Fixed the wrong index accessing code.

--- src/raft_server_backend_rocksdb.c
+++ src/raft_server_backend_rocksdb.c
@@ -482,11 +482,12 @@ rsbr_write_supplements_put(const struct raft_net_sm_write_supplements *ws,
 
         for (size_t j = 0; j < supp->rnws_nkv; j++)
         {
+
             supp->rnws_handle ?
             rocksdb_writebatch_put_cf(
                 wb, (rocksdb_column_family_handle_t *)supp->rnws_handle,
                 (const char *)supp->rnws_keys[j],
-                supp->rnws_key_sizes[i],
+                supp->rnws_key_sizes[j],
                 (const char *)supp->rnws_values[j],
                 supp->rnws_value_sizes[j])
                 :
