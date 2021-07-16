d1f510dea67da62c36b7fd23baa72741d0da4162
Lucusn <lucusn63@gmail.com>
2021-07-16 14:00:20 -0400

    commented out c_value check to get lkvt to work. plan to convert strings to bytes to they check can be ran still

--- go/pumiceDB/server/PumiceDBServer.go
+++ go/pumiceDB/server/PumiceDBServer.go
@@ -3,28 +3,28 @@ package PumiceDBServer
 import (
 	"errors"
 	"fmt"
-	"niova/go-pumicedb-lib/common"
 	"reflect"
 	"strconv"
 	"unsafe"
 
 	log "github.com/sirupsen/logrus"
+
+	/*
+	   #cgo LDFLAGS: -lniova -lniova_raft -lniova_pumice -lrocksdb
+	   #include <raft/pumice_db.h>
+	   #include <rocksdb/c.h>
+	   #include <raft/raft_net.h>
+	   #include <raft/pumice_db_client.h>
+	   extern void applyCgo(const struct raft_net_client_user_id *, const void *,
+	                        size_t, void *, void *);
+	   extern size_t readCgo(const struct raft_net_client_user_id *, const void *,
+	                       size_t, void *, size_t, void *);
+	*/
+
+	gopointer "github.com/mattn/go-pointer"
 )
 
-/*
-#cgo LDFLAGS: -lniova -lniova_raft -lniova_pumice -lrocksdb
-#include <raft/pumice_db.h>
-#include <rocksdb/c.h>
-#include <raft/raft_net.h>
-#include <raft/pumice_db_client.h>
-extern void applyCgo(const struct raft_net_client_user_id *, const void *,
-                     size_t, void *, void *);
-extern size_t readCgo(const struct raft_net_client_user_id *, const void *,
-                    size_t, void *, size_t, void *);
-*/
 import "C"
-
-import gopointer "github.com/mattn/go-pointer"
 
 type PmdbServerAPI interface {
 	Apply(unsafe.Pointer, unsafe.Pointer, int64, unsafe.Pointer)
@@ -215,26 +215,13 @@ func PmdbLookupKey(key string, key_len int64,
 	C.rocksdb_readoptions_destroy(ropts)
 
 	if C_value != nil {
-<<<<<<< HEAD
+
+		buffer_value := CToGoBytes(C_value, C.int(C_value_len))
+
+		log.Debug("C_value: ", C_value, " \nbuffer_value: ", buffer_value)
 
 		result = C.GoBytes(unsafe.Pointer(C_value), C.int(C_value_len))
 		lookup_err = nil
-=======
-		buffer_value := CToGoString(C_value)
-		buffer_value_len := int64(len(buffer_value))
-
-		return_value_len := CToGoInt64(C_value_len)
-
-		// Compare the buffer length with value length returned by C.rocksdb_get_cf
-		if buffer_value_len < return_value_len {
-			lookup_err = errors.New("Bad value")
-			log.Error("buffer value len: ", buffer_value_len, "return value len: ", return_value_len)
-			result = ""
-		} else {
-			result = C.GoStringN(C_value, C.int(C_value_len))
-			lookup_err = nil
-		}
->>>>>>> added patch 88 from manisha to PumiceDBServer.go. And set up niovakv_client.go with api
 		FreeCMem(C_value)
 	} else {
 		lookup_err = errors.New("Failed to lookup for key")
