1a1dad55a3bf52b914370d9905f53fe9068ad01f
Sudarsan <sudarsan.sivakumar@paroscale.com>
2021-08-12 10:28:38 +0000

    More optimized membership update

--- go/pumiceDB/examples/niovakv/niovakv_client/niovakv_client_api.go
+++ go/pumiceDB/examples/niovakv/niovakv_client/niovakv_client_api.go
@@ -4,7 +4,6 @@ import (
 	"math/rand"
 	"os"
 	"sync"
-	"sync/atomic"
 	"time"
 
 	log "github.com/sirupsen/logrus"
@@ -13,36 +12,37 @@ import (
 	"niovakv/niovakvlib"
 	"niovakv/serfclienthandler"
 
-	"github.com/hashicorp/serf/client"
+	client "github.com/hashicorp/serf/client"
 )
 
 type NiovakvClient struct {
-	addies         []addrStruct
-	ClientHandler  serfclienthandler.SerfClientHandler
-	addrTableLock  sync.Mutex
+	servers       []client.Member
+	clientHandler serfclienthandler.SerfClientHandler
+	//httpStatus     map[string]bool
 	serfUpdateLock sync.Mutex
+	tableLock      sync.Mutex
 }
 
-type addrStruct struct {
-	addr   string
-	failed *int32
+func getAddr(member *client.Member) string {
+	httpAddr := member.Addr.String() + ":" + member.Tags["Hport"]
+	return httpAddr
 }
 
 func (nkvc *NiovakvClient) Put(ReqObj *niovakvlib.NiovaKV) int {
 	ReqObj.InputOps = "write"
 	// nkvc.SerfClientInit()
-	toSendAddr := nkvc.pickServer()
+	toSend := nkvc.pickServer()
 	//Do upto 5 times if request failed
 	var responseRecvd *niovakvlib.NiovaKVResponse
 	for j := 0; j < 5; j++ {
 		var err error
-		responseRecvd, err = httpclient.PutRequest(ReqObj, toSendAddr.addr)
+		addr := getAddr(&toSend)
+		responseRecvd, err = httpclient.PutRequest(ReqObj, addr)
 		if err == nil {
 			break
 		}
 		log.Error(err)
-		atomic.AddInt32(toSendAddr.failed, 1)
-		toSendAddr = nkvc.pickServer()
+		toSend = nkvc.pickServer()
 	}
 	return responseRecvd.RespStatus
 }
@@ -50,25 +50,26 @@ func (nkvc *NiovakvClient) Put(ReqObj *niovakvlib.NiovaKV) int {
 func (nkvc *NiovakvClient) Get(ReqObj *niovakvlib.NiovaKV) []byte {
 	ReqObj.InputOps = "read"
 	// nkvc.SerfClientInit()
-	toSendAddr := nkvc.pickServer()
+	toSend := nkvc.pickServer()
+
 	//Do upto 5 times if request failed
 	var responseRecvd *niovakvlib.NiovaKVResponse
 	for j := 0; j < 5; j++ {
 		var err error
-		responseRecvd, err = httpclient.GetRequest(ReqObj, toSendAddr.addr)
+		addr := getAddr(&toSend)
+		responseRecvd, err = httpclient.GetRequest(ReqObj, addr)
 		if err == nil {
 			break
 		}
 		log.Error(err)
-		atomic.AddInt32(toSendAddr.failed, 1)
-		toSendAddr = nkvc.pickServer()
+		toSend = nkvc.pickServer()
 	}
 	return responseRecvd.RespValue
 }
 
 func (nkvc *NiovakvClient) SerfClientInit() {
-	nkvc.ClientHandler.Retries = 5
-	nkvc.ClientHandler.Initdata("../config")
+	nkvc.clientHandler.Retries = 5
+	nkvc.clientHandler.Initdata("../config")
 }
 
 func (nkvc *NiovakvClient) MemberSearcher(stop chan int) {
@@ -82,24 +83,15 @@ comparison:
 			log.Info("Member table update initiated")
 			//Since we do update it continuesly, we persist the connection
 			nkvc.serfUpdateLock.Lock()
-			err := nkvc.ClientHandler.GetData(true)
+			err := nkvc.clientHandler.GetData(true)
 			nkvc.serfUpdateLock.Unlock()
 			if err != nil {
 				log.Error("Unable to connect with agents")
 				os.Exit(1)
 			}
-			var addies []addrStruct
-			//Only alive addr will be there in Agents
-			for _, mems := range nkvc.ClientHandler.Agents {
-				a := int32(0)
-				addies = append(addies, addrStruct{
-					addr:   mems.Addr.String() + ":" + mems.Tags["Hport"],
-					failed: &a,
-				})
-			}
-			nkvc.addrTableLock.Lock()
-			nkvc.addies = addies
-			nkvc.addrTableLock.Unlock()
+			nkvc.tableLock.Lock()
+			nkvc.servers = nkvc.clientHandler.Agents
+			nkvc.tableLock.Unlock()
 			time.Sleep(2 * time.Second)
 		}
 	}
@@ -110,11 +102,11 @@ func (nkvc *NiovakvClient) Start(stop chan int) {
 	go nkvc.MemberSearcher(stop)
 }
 
-func (nkvc *NiovakvClient) pickServer() *addrStruct {
-	nkvc.addrTableLock.Lock()
-	defer nkvc.addrTableLock.Unlock()
+func (nkvc *NiovakvClient) pickServer() client.Member {
+	nkvc.tableLock.Lock()
+	defer nkvc.tableLock.Unlock()
 
-	if len(nkvc.addies) == 0 {
+	if len(nkvc.clientHandler.Agents) == 0 {
 		log.Error("no alive servers")
 		os.Exit(1)
 	}
@@ -122,29 +114,30 @@ func (nkvc *NiovakvClient) pickServer() *addrStruct {
 	//Get random addr and delete if its failed and provide with non-failed one!
 	var randomIndex int
 	for {
-		randomIndex = rand.Intn(len(nkvc.addies))
-		if atomic.LoadInt32(nkvc.addies[randomIndex].failed) == 0 {
+		randomIndex = rand.Intn(len(nkvc.servers))
+		if nkvc.servers[randomIndex].Status == "alive" {
 			break
 		}
-		nkvc.addies = removeIndex(nkvc.addies, randomIndex)
+		nkvc.servers = removeIndex(nkvc.servers, randomIndex)
 	}
 
-	return &nkvc.addies[randomIndex]
+	return nkvc.servers[randomIndex]
 }
 
 //Returns raft leader's uuid
 func (nkvc *NiovakvClient) GetLeader() string {
-	nkvc.serfUpdateLock.Lock()
-	defer nkvc.serfUpdateLock.Unlock()
-	return nkvc.ClientHandler.Agents[0].Tags["Leader UUID"]
+	agent := nkvc.pickServer()
+	return agent.Tags["Leader UUID"]
 }
 
 func (nkvc *NiovakvClient) GetMembership() map[string]client.Member {
-	return nkvc.ClientHandler.GetMemberListMap()
+	nkvc.serfUpdateLock.Lock()
+	defer nkvc.serfUpdateLock.Unlock()
+	return nkvc.clientHandler.GetMemberListMap()
 }
 
-func removeIndex(s []addrStruct, index int) []addrStruct {
-	ret := make([]addrStruct, 0)
+func removeIndex(s []client.Member, index int) []client.Member {
+	ret := make([]client.Member, 0)
 	ret = append(ret, s[:index]...)
 	return append(ret, s[index+1:]...)
 }
