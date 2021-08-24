bc0939ecd6a43d0fa9ccdc1f162593515e6cfab3
Manisha Salve <manisha@paroscale.com>
2021-08-24 04:56:58 +0000

    Allow passing app_type while running recipes.

--- scripts/run-recipes.sh
+++ scripts/run-recipes.sh
@@ -5,6 +5,7 @@ BIN_PATH=${2}
 LOG_PATH=${3}
 NPEERS=${4}
 RECIPE_FILE=${5}
+APP_TYPE=${6}
 
 export ANSIBLE_LOOKUP_PLUGINS=$HOLON_LIBS
 export PYTHONPATH=$HOLON_LIBS
@@ -16,7 +17,7 @@ done <$RECIPE_FILE
 
 for recipe in "${recipe_list[@]}"
 do
-   ansible-playbook -e 'srv_port=4000' -e npeers=$NPEERS -e dir_path=$LOG_PATH -e 'client_port=14000' -e recipe=$recipe -e 'backend_type=pumicedb' holon.yml
+   ansible-playbook -e 'srv_port=4000' -e npeers=$NPEERS -e dir_path=$LOG_PATH -e 'client_port=14000' -e recipe=$recipe -e 'backend_type=pumicedb' -e app_type=$APP_TYPE holon.yml
    if [ $? -ne 0 ]
    then
       echo "Recipe: $recipe failed"
