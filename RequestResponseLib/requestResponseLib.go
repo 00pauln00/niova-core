8bb426720cbca710c3fb81a8e2cab35bb6808c6c
Sudarsan <sudarsan.sivakumar@paroscale.com>
2022-01-13 13:23:09 +0000

    Added changes for nisd communication with lookout and providing status of nisd in gossip

---
+++ RequestResponseLib/requestResponseLib.go
@@ -0,0 +1,13 @@
+package requestResponseLib
+
+type KVRequest struct {
+	Operation  string
+	Key        string
+	Value      []byte
+	CheckSum   [16]byte
+}
+
+type KVResponse struct {
+	Status int
+	Value  []byte
+}
---
+++ http/client/go.mod
@@ -0,0 +1,5 @@
+module lookout/httpclient
+
+go 1.16
+
+require github.com/sirupsen/logrus v1.8.1
