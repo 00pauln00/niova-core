e641f59927097db3027737d10feccf569f815256
NikhilReddy-2 <nikhil.reddy@paroscale.com>
2022-02-28 14:38:00 +0530

    Added helper files

--- go/controlPlane/services/ncpc/ncpc.go
+++ go/controlPlane/services/ncpc/ncpc.go
@@ -3,8 +3,6 @@ package main
 import (
 	"bytes"
 	"common/requestResponseLib"
-	"common/clientAPI"
-	compressionLib "common/specificCompressionLib"
 	"encoding/gob"
 	"encoding/json"
 	"flag"
@@ -36,41 +34,6 @@ func (handler *clientHandler) getCmdParams() {
 	flag.StringVar(&handler.resultFile, "r", "operation", "Path along with file name for the result file")
 	flag.StringVar(&handler.rncui, "u", uuid.NewV4().String()+":0:0:0:1", "RNCUI for request")
 	flag.Parse()
-}
-
-//Write to Json
-func (cli *clientHandler) write2Json(toJson map[string][]opData) {
-	file, _ := json.MarshalIndent(toJson, "", " ")
-	_ = ioutil.WriteFile(cli.resultFile+".json", file, 0644)
-}
-
-func (cli *clientHandler) putNISDInfo() map[string]nisdData {
-	data := cli.clientAPIObj.GetMembership()
-	nisdDataMap := make(map[string]nisdData)
-	for _, node := range data {
-		if (node.Tags["Type"] == "LOOKOUT") && (node.Status == "alive") {
-			for cuuid, value := range node.Tags {
-				uuid, err := compressionLib.DecompressUUID(cuuid)
-				if err != nil {
-					CompressedStatus := value[1]
-					CompressedWriteMeta := value[1:3]
-
-					//Decompress
-					thisNISDData := nisdData{}
-					thisNISDData.UUID = uuid
-					if string(CompressedStatus) == "1" {
-						thisNISDData.Status = "Alive"
-					} else {
-						thisNISDData.Status = "Dead"
-					}
-
-					thisNISDData.WriteSize = compressionLib.DecompressNumber(CompressedWriteMeta)
-					nisdDataMap[uuid] = thisNISDData
-				}
-			}
-		}
-	}
-	return nisdDataMap
 }
 
 func main() {
---
+++ go/controlPlane/services/proxy/helper.go
@@ -0,0 +1,56 @@
+package proxy
+
+import (
+	"os"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func getAnyEntryFromStringMap(mapSample map[string]map[string]string) map[string]string {
+	for _, v := range mapSample {
+		return v
+	}
+	return nil
+}
+
+func (handler *proxyHandler) dumpConfigToFile(outfilepath string) error {
+	//Generate .raft
+	raft_file, err := os.Create(outfilepath + handler.raftUUID + ".raft")
+	if err != nil {
+		return err
+	}
+
+	_, errFile := raft_file.WriteString("RAFT " + handler.raftUUID + "\n")
+	if errFile != nil {
+		return err
+	}
+
+	for _, peer := range handler.PMDBServerConfigArray {
+		raft_file.WriteString("PEER " + peer.PeerUUID + "\n")
+	}
+
+	raft_file.Sync()
+	raft_file.Close()
+
+	//Generate .peer
+	for _, peer := range handler.PMDBServerConfigArray {
+		peer_file, err := os.Create(outfilepath + peer.PeerUUID + ".peer")
+		if err != nil {
+			log.Error(err)
+		}
+
+		_, errFile := peer_file.WriteString(
+			"RAFT         " + handler.raftUUID +
+				"\nIPADDR       " + peer.IPAddr +
+				"\nPORT         " + peer.Port +
+				"\nCLIENT_PORT  " + peer.ClientPort +
+				"\nSTORE        ./*.raftdb\n")
+
+		if errFile != nil {
+			return errFile
+		}
+		peer_file.Sync()
+		peer_file.Close()
+	}
+	return nil
+}
