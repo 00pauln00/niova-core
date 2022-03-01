f0d49c490e898cff82ef73df310920ae84301620
Sudarsan <sudarsan.sivakumar@paroscale.com>
2022-03-01 13:27:09 +0000

    Bug fixes in gossip decoding in proxy

--- go/controlPlane/proxy/proxy.go
+++ go/controlPlane/proxy/proxy.go
@@ -239,14 +239,15 @@ func (handler *proxyHandler) GetPMDBServerConfig() error {
 		uuid, err := compressionLib.DecompressUUID(key)
 		if err == nil {
 			IPAddr := compressionLib.DecompressIPV4(value[:4])
-			Port := compressionLib.DecompressNumber(value[4:6])
-			ClientPort := compressionLib.DecompressNumber(value[6:8])
+			Port := compressionLib.DecompressNumber(value[4:7])
+			ClientPort := compressionLib.DecompressNumber(value[7:10])
 			peerConfig := PeerConfigData{
 				PeerUUID:   uuid,
 				IPAddr:     IPAddr,
 				Port:       Port,
 				ClientPort: ClientPort,
 			}
+			log.Info("Peer config : ", peerConfig)
 			handler.PMDBServerConfigArray = append(handler.PMDBServerConfigArray, peerConfig)
 			handler.PMDBServerConfigByteMap[uuid], _ = json.Marshal(peerConfig)
 		}
