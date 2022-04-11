b75bf423e399eb8bde91c6bdb2c6d74997628923
Sudarsan <sudarsan.sivakumar@paroscale.com>
2022-04-11 05:36:18 +0000

    Resolved conflict in ncpc

--- go/controlPlane/ncpc/ncpc.go
+++ go/controlPlane/ncpc/ncpc.go
@@ -172,13 +172,9 @@ func main() {
 		var requestByte bytes.Buffer
 		enc := gob.NewEncoder(&requestByte)
 		enc.Encode(requestObj)
-<<<<<<< Updated upstream
-		//Send the write
-		responseBytes := clientObj.clientAPIObj.Request(requestByte.Bytes(), "", write)
-=======
+
 		//Send the request
-		responseBytes := clientObj.clientAPIObj.Request(requestByte.Bytes(), "", passNext)
->>>>>>> Stashed changes
+		responseBytes := clientObj.clientAPIObj.Request(requestByte.Bytes(), "", write)
 		dec := gob.NewDecoder(bytes.NewBuffer(responseBytes))
 		err = dec.Decode(&responseObj)
 		fmt.Println("Response:", string(responseObj.Value))
