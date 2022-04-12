c743aa47d3977ac1db61d5249b4bfae5f4f93a83
Sudarsan <sudarsan.sivakumar@paroscale.com>
2022-04-12 16:16:48 +0000

    Request response structure change

--- go/controlPlane/requestResponseLib/requestResponseLib.go
+++ go/controlPlane/requestResponseLib/requestResponseLib.go
@@ -3,12 +3,16 @@ package requestResponseLib
 type KVRequest struct {
 	Operation string
 	Key       string
+	LastKey   string
 	Value     []byte
 	Rncui     string
 	CheckSum  [16]byte
 }
 
 type KVResponse struct {
-	Status int
-	Value  []byte
+	Status		int
+	Value		[]byte
+	RangeMap	map[string]string
+	ContinueRead	bool
+	LastKey		string
 }
