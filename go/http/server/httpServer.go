53b6b344586c8612ddbe4e3fa370ca9038efafb1
Lucus Nowoczynski <nowoczyn001@gannon.edu>
2022-05-18 20:24:57 -0400

    added timer and maps to display stats with json

--- go/http/server/httpServer.go
+++ go/http/server/httpServer.go
@@ -28,23 +28,41 @@ type HTTPServerHandler struct {
 	StatsRequired bool
 	Stat          HTTPServerStat
 	statLock      sync.Mutex
+	Buckets		  int64
 }
 
 type HTTPServerStat struct {
-	GetCount        int64
-	PutCount        int64
-	GetSuccessCount int64
-	PutSuccessCount int64
-	Queued          int64
-	ReceivedCount   int64
-	FinishedCount   int64
-	syncRequest     int64
-	StatusMap       map[int64]*RequestStatus
+	GetCount        		int64
+	PutCount        		int64
+	GetSuccessCount 		int64
+	PutSuccessCount 		int64
+	Queued          		int64
+	ReceivedCount   		int64
+	FinishedCount   		int64
+	syncRequest     		int64
+	receiveStats			opInfo
+	sendStats				opInfo
+	readStats				opInfo
+	writeStats				opInfo
+	ReceiveTimeMicroSec		map[string]int64
+	ReceiveSize				map[string]int64
+	SendTimeMicroSec		map[string]int64
+	SendSize				map[string]int64
+	ReadTimeMicroSec		map[string]int64
+	ReadSize				map[string]int64
+	WriteTimeMicroSec		map[string]int64
+	WriteSize				map[string]int64
+	StatusMap       		map[int64]*RequestStatus
 }
 
 type RequestStatus struct {
 	RequestHash string
 	Status      string
+}
+
+type opInfo struct{
+	OpTimeMicroSec	[]int64
+	RequestSize		[]int64
 }
 
 func (handler *HTTPServerHandler) configHandler(writer http.ResponseWriter, reader *http.Request) {
@@ -127,15 +145,22 @@ func (handler *HTTPServerHandler) kvRequestHandler(writer http.ResponseWriter, r
 	var read bool
 	var result []byte
 	var err error
-
+	//start timer
+	reqTime:=time.Now()
 	//Handle the KV request
 	requestBytes, err := ioutil.ReadAll(reader.Body)
+	//get read all time
+	handler.timeTrack("REC",len(requestBytes),reqTime)
+
+	opTime:=time.Now()
 	switch reader.Method {
 	case "GET":
 		if handler.StatsRequired {
 			thisRequestStat.Status = "Processing"
 		}
 		err = handler.GETHandler(requestBytes, &result)
+		//get get time
+		handler.timeTrack("GET",len(requestBytes),opTime)
 		read = true
 		fallthrough
 	case "PUT":
@@ -144,11 +169,15 @@ func (handler *HTTPServerHandler) kvRequestHandler(writer http.ResponseWriter, r
 				thisRequestStat.Status = "Processing"
 			}
 			err = handler.PUTHandler(requestBytes)
+			//get put time
+			handler.timeTrack("PUT",len(requestBytes),opTime)
 		}
 		if err == nil {
 			success = true
 		}
+		sendTime:=time.Now()
 		fmt.Fprintf(writer, "%s", string(result))
+		handler.timeTrack("SEN",len(requestBytes),sendTime)
 	default:
 		writer.WriteHeader(http.StatusMethodNotAllowed)
 	}
@@ -159,10 +188,77 @@ func (handler *HTTPServerHandler) kvRequestHandler(writer http.ResponseWriter, r
 	}
 }
 
+//timer for operations
+func (handler *HTTPServerHandler) timeTrack(op string, size int, timer time.Time){
+	opTime := time.Since(timer)
+	switch op {
+		case "GET":
+			handler.histPlace(handler.Stat.readStats.RequestSize,size)
+			handler.histPlace(handler.Stat.readStats.OpTimeMicroSec,int(opTime.Microseconds()))
+		case "PUT":
+			handler.histPlace(handler.Stat.writeStats.RequestSize,size)
+			handler.histPlace(handler.Stat.writeStats.OpTimeMicroSec,int(opTime.Microseconds()))
+		case "REC":
+			handler.histPlace(handler.Stat.receiveStats.RequestSize,size)
+			handler.histPlace(handler.Stat.receiveStats.OpTimeMicroSec,int(opTime.Microseconds()))
+		case "SEN":
+			handler.histPlace(handler.Stat.sendStats.RequestSize,size)
+			handler.histPlace(handler.Stat.sendStats.OpTimeMicroSec,int(opTime.Microseconds()))
+		default:
+		
+	}
+}
+
+//increment count on correct position of histogram
+func (handler *HTTPServerHandler) histPlace(h []int64, value int){
+	v := 2
+	added := false
+	buckets:=handler.Buckets
+	for i := 0; i < int(buckets)-1; i++ {
+		if v >= value{
+			h[i]++
+			added=true
+			break
+		}
+		v=v*2
+	}
+	if !added {
+		h[buckets-1]++
+	}
+}
+
+//create histogram with num of buckets and add number value to each bucket value
+func (handler *HTTPServerHandler) makehist() []int64{
+	buckets:=handler.Buckets
+	h := make([]int64, buckets)
+	return h
+}
+
+func (handler *HTTPServerHandler) makemap(ar []int64) map[string]int64{
+	m := make(map[string]int64)
+	v:=2
+	for i := 0; i < len(handler.Stat.writeStats.OpTimeMicroSec)-1; i++ {
+		zeroPad:=fmt.Sprintf("%08d",v)
+		m[zeroPad]=ar[i]
+		v=v*2
+		if i== len(handler.Stat.writeStats.OpTimeMicroSec)-2{
+			m[zeroPad+"+"]=ar[i+1]
+		}
+	}
+	return m
+}
 //HTTP server handler called when request is received
 func (handler *HTTPServerHandler) ServeHTTP(writer http.ResponseWriter, reader *http.Request) {
 	//Go follows causually consistent memory model, so require sync among stat and normal request to get consistent stat data
 	//atomic.AddInt64(&handler.Stat.syncRequest,int64(1))
+	handler.Stat.ReceiveTimeMicroSec=handler.makemap(handler.Stat.receiveStats.OpTimeMicroSec)
+	handler.Stat.ReceiveSize=handler.makemap(handler.Stat.receiveStats.RequestSize)
+	handler.Stat.SendTimeMicroSec=handler.makemap(handler.Stat.sendStats.OpTimeMicroSec)
+	handler.Stat.SendSize=handler.makemap(handler.Stat.sendStats.RequestSize)
+	handler.Stat.ReadTimeMicroSec=handler.makemap(handler.Stat.readStats.OpTimeMicroSec)
+	handler.Stat.ReadSize=handler.makemap(handler.Stat.readStats.RequestSize)
+	handler.Stat.WriteTimeMicroSec=handler.makemap(handler.Stat.writeStats.OpTimeMicroSec)
+	handler.Stat.WriteSize=handler.makemap(handler.Stat.writeStats.RequestSize)
 	if reader.URL.Path == "/config" {
 		handler.configHandler(writer, reader)
 	} else if (reader.URL.Path == "/stat") && (handler.StatsRequired) {
@@ -177,6 +273,16 @@ func (handler *HTTPServerHandler) Start_HTTPServer() error {
 	handler.connectionLimiter = make(chan int, handler.HTTPConnectionLimit)
 	handler.HTTPServer = http.Server{}
 	handler.HTTPServer.Addr = handler.Addr.String() + ":" + handler.Port
+
+	//make histograms
+	handler.Stat.receiveStats.OpTimeMicroSec=handler.makehist()
+	handler.Stat.receiveStats.RequestSize=handler.makehist()
+	handler.Stat.sendStats.OpTimeMicroSec=handler.makehist()
+	handler.Stat.sendStats.RequestSize=handler.makehist()
+	handler.Stat.readStats.OpTimeMicroSec=handler.makehist()
+	handler.Stat.readStats.RequestSize=handler.makehist()
+	handler.Stat.writeStats.OpTimeMicroSec=handler.makehist()
+	handler.Stat.writeStats.RequestSize=handler.makehist()
 
 	//Update the timeout using little's fourmula
 	handler.HTTPServer.Handler = http.TimeoutHandler(handler, 150*time.Second, "Server Timeout")
