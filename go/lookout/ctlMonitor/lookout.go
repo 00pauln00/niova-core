bbcba6f9a18673c016e0cc05d76071e529657489
ashwini-8 <ashwini.patil@paroscale.com>
2022-09-22 16:44:21 +0000

    Bug fix in lookout and ctl_interface.go
    It was not able to get the lookoutInfo properly,
    added code for the same.

--- go/lookout/ctlMonitor/lookout.go
+++ go/lookout/ctlMonitor/lookout.go
@@ -267,7 +267,6 @@ func (epc *EPContainer) customQuery(node uuid.UUID, query string) []byte {
 	epc.mutex.Lock()
 	ep := epc.EpMap[node]
 	epc.mutex.Unlock()
-
 	//If not present
 	if ep == nil {
 		return []byte("Specified NISD is not present")
@@ -275,7 +274,7 @@ func (epc *EPContainer) customQuery(node uuid.UUID, query string) []byte {
 
 	httpID := "HTTP_" + uuid.New().String()
 	epc.httpQuery[httpID] = make(chan []byte, 2)
-	ep.CustomQuery(query, httpID)
+	ep.CtlCustomQuery(query, httpID)
 
 	//FIXME: Have select in case of NISD dead and delete the channel
 	var byteOP []byte
@@ -303,7 +302,6 @@ func (epc *EPContainer) QueryHandle(w http.ResponseWriter, r *http.Request) {
 
 	//Call the appropriate function
 	output := epc.customQuery(requestObj.UUID, requestObj.Cmd)
-
 	//Data to writer
 	w.Write(output)
 }
@@ -391,6 +389,7 @@ func (epc *EPContainer) MarkAlive(serviceUUID string) error {
 func (epc *EPContainer) Start() {
 	//Start http service
 	if epc.EnableHttp {
+		epc.httpQuery = make(map[string](chan []byte))
 		go epc.serveHttp()
 	}
 
