00pauln00/niova-core

commit 1a5eb1dbb4e651a255bd63357d282a956901a93e

GC thread check changes

go/pumiceDB/lease/server/leaseServer.go
@@ -625,11 +625,12 @@ func (lso *LeaseServerObject) leaseGarbageCollector() {
 			if cobj, ok := e.Value.(*leaseLib.LeaseInfo); ok {
 				//Check lease status before processing it.
 				obj := cobj.LeaseMetaInfo
-				if obj.LeaseState != leaseLib.GRANTED &&
-					obj.LeaseState != leaseLib.STALE_INPROGRESS &&
-					obj.LeaseState != leaseLib.EXPIRED_LOCALLY {
+				//We dont mark lease as stale if they are expired
+				//or inprogress
+				if (obj.LeaseState == leaseLib.EXPIRED) &&
+				   (obj.LeaseState == leaseLib.INPROGRESS) {
 					continue
-				}
+				} 
 				if lso.isExpired(obj.TimeStamp) {
 					cobj.LeaseMetaInfo.LeaseState = leaseLib.STALE_INPROGRESS
 					log.Trace("Enqueue lease for stale lease processing: ",

commit 9822acf727530f8cd390e01dbd5fabddf06fa2bc

Fixed condition check regression from 1a5eb1dbb

go/pumiceDB/lease/server/leaseServer.go
@@ -627,7 +627,7 @@ func (lso *LeaseServerObject) leaseGarbageCollector() {
 				obj := cobj.LeaseMetaInfo
 				//We dont mark lease as stale if they are expired
 				//or inprogress
-				if (obj.LeaseState == leaseLib.EXPIRED) &&
+				if (obj.LeaseState == leaseLib.EXPIRED) ||
 				   (obj.LeaseState == leaseLib.INPROGRESS) {
 					continue
 				} 