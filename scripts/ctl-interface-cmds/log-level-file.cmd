0b78ad2fa754c9c49d1682b23ac632c36adc9171
Paul Nowoczynski <pauln@niova.io>
2023-08-11 13:09:47 -0400

    Add ctl-interface cmds for log level changes

---
+++ scripts/ctl-interface-cmds/log-level-file.cmd
@@ -0,0 +1,3 @@
+APPLY file_log_level@warn
+WHERE /log_entry_map/file_name@src\/chunk_handle.c
+OUTFILE /file-ll-err.out
---
+++ scripts/ctl-interface-cmds/log-level-function.cmd
@@ -0,0 +1,3 @@
+APPLY log_level@warn
+WHERE /log_entry_map/log_entries/function@nisd_mb_mfve_occlude_check
+OUTFILE /func-ll-err.out
