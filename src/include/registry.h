0a7028b9220bf48eb60ca58adbe72f34675dca1f
Paul Nowoczynski <pauln@niova.io>
2025-10-13 16:19:15 -0400

    Add lri_disallow_other_threads to struct lreg_instance
    For possible use of private lreg instances.

--- src/include/registry.h
+++ src/include/registry.h
@@ -355,8 +355,10 @@ struct lreg_instance
     struct lreg_node_list     lri_installq;
     struct lreg_destroy_queue lri_destroyq;
     uint8_t                   lri_init:1;
+    uint8_t                   lri_disallow_other_threads:1;
     int                       lri_eventfd;
     pthread_t                 lri_pthread;
+    int64_t                   lri_ref_cnt; // protected by LREG_SUBSYS_LOCK
     struct epoll_handle      *lri_eph;
 };
 
